511ee722d4213ec6875e8401e17442d0261880df
chenmengjie <chenmengjie@xiaomi.com>
2018-08-26 22:31:46 +0800

    hook方法

---
+++ FluentPython/no_3/namedtupleDemo.py
@@ -0,0 +1,57 @@
+import collections
+
+Grade = collections.namedtuple('Grade', ('score', 'weight'))
+
+
+# 课目的类
+class Subject(object):
+    def __init__(self):
+        self._grades = []
+
+    def report_grade(self, score, weight):
+        self._grades.append(Grade(score, weight))
+
+    def average_grade(self):
+        total, total_weight = 0, 0
+        for grade in self._grades:
+            total += grade.score * grade.weight
+            total_weight += grade.weight
+        return total / total_weight
+
+
+# 学生类
+class Student(object):
+    def __init__(self):
+        self._subjects = {}
+
+    def subject(self, name):
+        if name not in self._subjects:
+            self._subjects[name] = Subject()
+        return self._subjects[name]
+
+    def average_grade(self):
+        total, count = 0, 0
+        for subject in self._subjects.values():
+            total += subject.average_grade()
+            count += 1
+        return total / count
+
+
+# 所有学生考试的容器
+class Gradebook(object):
+    def __init__(self):
+        self._students = {}
+
+    def student(self, name):
+        if name not in self._students:
+            self._students[name] = Student()
+        return self._students[name]
+
+
+if __name__ == '__main__':
+    book = Gradebook()
+    albert = book.student('Albert Einstein')
+    math = albert.subject('Math')
+    math.report_grade(80, 0.10)
+
+    print(albert.average_grade())
